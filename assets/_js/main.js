800389b1276ed3d1da9f26977c5bcea443dd04fa
Nathan Henniges <demonwolf@demonwolfdev.com>
2017-11-29 20:57:49 -0600

    Added work content.

--- assets/_js/main.js
+++ assets/_js/main.js
@@ -1,36 +1,36 @@
 // Fixs the menu so when link is clicked it would close the menu.
 $(function () {
 
-    $('#bs-example-navbar-collapse-1')
-        .on('shown.bs.collapse', function () {
-            $('#navbar-hamburger').addClass('hidden');
-            $('#navbar-close').removeClass('hidden');
-        })
-        .on('hidden.bs.collapse', function () {
-            $('#navbar-hamburger').removeClass('hidden');
-            $('#navbar-close').addClass('hidden');
-        });
+  $('#bs-example-navbar-collapse-1')
+    .on('shown.bs.collapse', function () {
+      $('#navbar-hamburger').addClass('hidden');
+      $('#navbar-close').removeClass('hidden');
+    })
+    .on('hidden.bs.collapse', function () {
+      $('#navbar-hamburger').removeClass('hidden');
+      $('#navbar-close').addClass('hidden');
+    });
 
 });
 
 $(document).on('click', '.navbar-collapse.in', function (e) {
-    if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
-        $(this).collapse('hide');
-    }
+  if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle') {
+    $(this).collapse('hide');
+  }
 });
 $(function () {
-    $(document).click(function (event) {
-        $('.navbar-collapse').collapse('hide');
-    });
+  $(document).click(function (event) {
+    $('.navbar-collapse').collapse('hide');
+  });
 });
 
 $(document).ready(function () {
-    setTimeout(function () {
-        $("#cookieConsent").fadeIn(200);
-    }, 4000);
-    $("#closeCookieConsent, .cookieConsentOK").click(function () {
-        $("#cookieConsent").fadeOut(200);
-    });
+  setTimeout(function () {
+    $("#cookieConsent").fadeIn(200);
+  }, 4000);
+  $("#closeCookieConsent, .cookieConsentOK").click(function () {
+    $("#cookieConsent").fadeOut(200);
+  });
 });
 
 (function () {
@@ -61,3 +61,28 @@ $(document).ready(function () {
   };
 
 })();
+// Project Gallery
+$(document).ready(function () {
+
+  $(".work_filter-button").click(function () {
+    var value = $(this).attr('data-filter');
+
+    if (value == "all") {
+      //$('.filter').removeClass('hidden');
+      $('.filter').show('1000');
+    }
+    else {
+      //$('.filter[filter-item="'+value+'"]').removeClass('hidden');
+      //$(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+      $(".filter").not('.' + value).hide('3000');
+      $('.filter').filter('.' + value).show('3000');
+
+    }
+  });
+
+  if ($(".work_gallery_filter-button").removeClass("active")) {
+    $(this).removeClass("active");
+  }
+  $(this).addClass("active");
+
+});
